0a3471c2b37fb46a0510c15c29e3da0f3f6684a6
Maikel Sleebos <mjsleebos@gmail.com>
2019-06-24 21:30:37 +0200

    feat(graphs): New functions to alter data

--- app/views/components/graphy/graphy.js
+++ app/views/components/graphy/graphy.js
@@ -1,10 +1,11 @@
-const graphy = ( graph,barN,newValue )=>{
+const graphy = graph=>{
 	const graphY = graph.querySelector( '.graph__y' )
 	const graphBars = graph.querySelector( '.graph__bars' )
-	const minVal = Number( graphY.getAttribute( 'data-min' ) )
+	// const minVal = Number( graphY.getAttribute( 'data-min' ) )
 	const maxVal = Number( graphY.getAttribute( 'data-max' ) )
 	const stepVal = Number( graphY.getAttribute( 'data-step' ) )
-    
+	
+	// Generate grid
 	for ( let i = 0; i < ( maxVal/stepVal )+1; i++ ) {
 		const newDiv = document.createElement( 'div' )
 
@@ -17,14 +18,14 @@ const graphy = ( graph,barN,newValue )=>{
 	for ( let i = 0; i < 7; i++ ) {
 		const newBar = document.createElement( 'div' )
 
-		if( i==0 ){
+		if( i===0 ){
 			const progress = document.createElement( 'div' )
             
 			progress.classList.add( 'bar__progress' )
 			newBar.appendChild( progress )
-		} else{
-			newBar.setAttribute( 'style',`height:${( Math.floor( Math.random()*5 ) )*40}px` )
-		}
+		} 
+
+		newBar.setAttribute( 'style',`height:${ 40 * ( i < 4 ? 3 : 5 ) }px` )
         
 		newBar.classList.add( 'bars__bar' )
 
@@ -32,4 +33,17 @@ const graphy = ( graph,barN,newValue )=>{
 	}
 }
 
-export {graphy}
+const updateBar = ( n,step,progValue )=>{
+	const graph = document.querySelector( '.graph' )
+	const graphBars = graph.querySelector( '.graph__bars' )
+	const bars = graphBars.querySelectorAll( '.bars__bar' )
+
+	bars[n].setAttribute( 'style',`height:${step * 40}px` )
+	
+	if( progValue ){
+		bars[n].querySelector( '.bar__progress' ).setAttribute( 'style',`height:${progValue + 40}px;` )
+	}
+
+}	
+
+export { graphy,updateBar  }
--- app/views/index.js
+++ app/views/index.js
@@ -1,11 +1,26 @@
 import { spendable } from './components/spendable/spendable.js'
-import { graphy } from './components/graphy/graphy.js'
+import { graphy,updateBar } from './components/graphy/graphy.js'
 spendable( document.querySelector( '.spendables .spendable:last-of-type' ),1033,500,350 )
 graphy( document.querySelector( '.graph' ) )
 
-setInterval(
+const ball_1 = document.querySelector( '.spendables .spendable:first-of-type' )
+const ball_2 = document.querySelector( '.spendables .spendable:last-of-type' )
+
+spendable( ball_1,1000,500 ) // Element,Total Value,New Value,Float value (not required)
+spendable( ball_2,1500,1500,300 )
+
+setTimeout(
 	()=>{
-		spendable( document.querySelector( '.spendables .spendable:last-of-type' ),1033,Math.floor( Math.random()*1033 ),Math.floor( Math.random()*350 ) )
-	}
-	,1000
+		spendable( ball_2,1500,1000,300 )
+		spendable( ball_1,1000,450 )
+		updateBar( 5,2 )
+	},
+	2000
+)
+
+setTimeout(
+	()=>{
+		updateBar( 0,3,2 )
+	},
+	5000
 )
